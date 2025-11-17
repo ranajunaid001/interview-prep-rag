3141dbe5be1d6791d14d812c2d9de976f3713b0a
ranajunaid001 <130623056+ranajunaid001@users.noreply.github.com>
2025-11-16 20:14:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,6 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return HTMLResponse(content="<h1>Interview Prep RAG</h1><p>System is running</p>")
+    with open("index.html", "r") as f:
+        html_content = f.read()
+    return HTMLResponse(content=html_content)
